d8d73351c417104d3859a34e001667e9261c82b1
Max Rogers <maxrogers90@gmail.com>
2014-01-04 15:59:21 -0500

    adding initalized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dads2::Application.config.secret_token = 'dc6c41215934efcb956d369ba9dff396a77c5dfffa93f46fda3c65c02c4a13becd2e6d4334b0b440288155f833a6941be6fc18f5cde9124474e9d29d7e0d9fbb'
